76fa8b95084343af07556fce02f85046b7cb8483
wintakers <wintakers@gmail.com>
2019-10-18 00:12:04 +0700

    fix

--- index.js
+++ index.js
@@ -64,52 +64,52 @@ async function handleMessageEvent(event) {
     let eventText = event.message.text.toLowerCase();
 
     if (eventText === 'ขอที่อยู่') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: address.MSG
         }
     } else if (eventText === 'สอบถาม') {
         // console.dir();
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
         }
     }  else if (eventText === 'สอบถามหน่อยครับ') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
 
         }
     } else if (eventText === 'สอบถามหน่อยค่ะ') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
 
         }
     } else if (eventText === 'ถามไรหน่อย') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
 
         }
     } else if (eventText === 'สวัสดีครับ') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
         }
     } else if (eventText === 'สวัสดีค่ะ') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
 
         }
     } else if (eventText === 'สวัสดี') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
         }
     } else if (eventText === 'หวัดดี') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: query.MSG
 
@@ -202,7 +202,12 @@ async function handleMessageEvent(event) {
              console.log(JSON.stringify(row));
            }
            data.id=JSON.stringify(result)
+           let  msg={
+            'type':'text',
+            'text': data.id
+        }
            console.log(`this is = ${result}`);
+           return client.replyMessage(event.replyToken, msg);
          });
         request({
             method: 'POST',
@@ -224,85 +229,82 @@ async function handleMessageEvent(event) {
             }
         })
 
-        msg={
-            'type':'text',
-            'text': data.id
-        }
+    
         
     } else if (eventText === 'ทุนวิจัย') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: capital.MSG
 
         }
     } else if (eventText === 'ขอรายละเอียดทุนวิจัย') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: capital.MSG1
 
         }
     } else if (eventText === 'ทุนวิจัย2564') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: capital.MSG2
 
         }
     } else if (eventText === 'ทุนวิจัย2563') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: capital.MSG3
 
         }
     } else if (eventText === 'เบิกเงินวิจัย') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: withdraw.MSG
         }
     } else if (eventText === 'ขอรายละเอียดเบิกเงินวิจัย') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: withdraw.MSG1
 
         }
     } else if (eventText === 'เบิกเงินงวดที่1') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: withdraw.MSG2
 
         }
     } else if (eventText === 'เบิกเงินงวดที่2') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: withdraw.MSG3
 
         }
     } else if (eventText === 'เบิกเงินงวดที่3') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: withdraw.MSG4
 
         }
     } else if (eventText === 'กองทุนสนับสนุนงานวิจัย') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: fund.MSG
 
         }
     } else if (eventText === 'คุยกับบอท') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: 'สวัสดีค่ะท่านสามารถสอบถามเกี่ยวกับ\n-ทุนวิจัย\n-เบิกเงินวิจัย\n-กองทุนสนับสนุนงานวิจัย\n-เอกสารดาวน์โหลด\n' +
                 'ท่านสามารถดูรายละเอียดโดยการพิมพ์ขอรายละเอียดแต่ละหัวข้อกับน้องบอทได้ เช่น ทุนวิจัย เป็นต้น'
 
         }
     } else if (eventText === 'สามารถติดต่อได้ทางไหนบ้าง') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: 'สวัสดีค่ะท่านสามารถติดต่อ สถาบันวิจัยและพัฒนา มทร.รัตนโกสินทร์ ได้ตามช่องทางการติดต่อด้านล่างนี้\nFacebook : https://www.facebook.com/irdrmutr\nWebsite : https://ird.rmutr.ac.th\nEmail : ird.r@rmutr.ac.th , irdrmutr@hotmail.co.th\nสามารถติดต่อได้ที่ 02-441-6060 ต่อ 2420-2426'
 
         }
     } else if (eventText === 'เอกสารดาวน์โหลด') {
-        msg = {
+       let msg = {
             'type': 'text',
             text: 'สวัสดีค่ะท่านสามารถดาวน์โหลดเอกสารต่างๆได้ในลิงค์ด้านล่างนี้\nhttps://ird.rmutr.ac.th/formdownload/ '
 
